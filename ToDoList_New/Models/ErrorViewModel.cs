d3bab82083d93e3cf82b21e8420a9e640c65fdfa
“ZhouTingting-TW” <tingting.zhou@thoughtworks.com>
2022-09-23 10:15:17 +0800

    feat: add ToDo model

--- ToDoList_New/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ToDoList_New.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ ToDoList_New/Models/ToDo.cs
@@ -0,0 +1,10 @@
+namespace ToDoList_New.Models;
+
+public class ToDo
+{
+    
+    public int Id { get; set; }
+    public string TaskName { get; set; }
+    public bool IsDone { get; set; }
+   
+}
