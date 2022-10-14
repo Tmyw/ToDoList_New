fdff724c0cadd40185a248ba34f6962d0cfd139b
ZhouTingting-TW <tingting.zhou@thoughtworks.com>
2022-10-14 16:05:10 +0800

    delete useless notes

--- ToDoList_New/Models/ToDo.cs
+++ ToDoList_New/Models/ToDo.cs
@@ -5,11 +5,7 @@ namespace ToDoList_New.Models;
 
 public class ToDo
 {
-    
-    // [Key]  
-    // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    [Required]public int Id { get; set; }
-   [Required, MaxLength(500)]  public string TaskName { get; set; } = null!;
-   [Required]public bool IsDone { get; set; }
-   
+    [Required] public int Id { get; set; }
+    [Required, MaxLength(500)] public string TaskName { get; set; } = null!;
+    [Required] public bool IsDone { get; set; }
 }
